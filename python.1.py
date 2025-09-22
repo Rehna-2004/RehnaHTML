41f230aa47410411c85ea5d0a696b847e6c85a35
Rehna-2004 <rehnampta@gmail.com>
2025-09-22 12:12:03 +0530

    Add files via upload

---
+++ python.1.py
@@ -0,0 +1,27 @@
+#list
+fruit=['apple','grape','orange']
+print(fruit)
+print(fruit[1])
+#adding item to a list
+fruit.append('pineapple')
+print(fruit)
+print(fruit[3])
+#insert
+fruit.insert(0,'banana')
+print(fruit)
+f=['guava','pomegranate','strawberry']
+#extend
+fruit.extend(f)
+print(fruit)
+#string
+f1='kiwi'
+fruit.extend(f1)
+print(fruit)
+
+#tupple
+vegetable=('tomato','onion','brinjal')
+print(vegetable)
+print(vegetable[2])
+print(vegetable[0])
+vegetable[1]="chilly"
+
