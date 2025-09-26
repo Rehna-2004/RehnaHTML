d9c6eaca959b3cb45920fb8cd8bebe861d102286
Rehna-2004 <rehnampta@gmail.com>
2025-09-25 23:57:05 -0700

    Add files via upload

--- python.1.py
+++ python.1.py
@@ -1,27 +1,19 @@
-#list
-fruit=['apple','grape','orange']
-print(fruit)
-print(fruit[1])
-#adding item to a list
-fruit.append('pineapple')
-print(fruit)
-print(fruit[3])
-#insert
-fruit.insert(0,'banana')
-print(fruit)
-f=['guava','pomegranate','strawberry']
-#extend
-fruit.extend(f)
-print(fruit)
-#string
-f1='kiwi'
-fruit.extend(f1)
-print(fruit)
+string=input("Enter a  string:")
+counts=dict()
+words=string.split()
+for i in words:
+    if i in counts:
+         counts[i]+=1
+    else:
+         counts[i]=1
+print(counts)
+for k,v in counts.items():
+    print(k,v)
+        
+         
+         
+        
 
-#tupple
-vegetable=('tomato','onion','brinjal')
-print(vegetable)
-print(vegetable[2])
-print(vegetable[0])
-vegetable[1]="chilly"
 
+    
+ 
