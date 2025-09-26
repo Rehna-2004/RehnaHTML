cce700b43136911b6207d621d915a0ad060d58ec
Rehna-2004 <rehnampta@gmail.com>
2025-09-25 23:58:13 -0700

    Add files via upload

--- python.2.py
+++ python.2.py
@@ -1,2 +1,10 @@
-#dict
-dict={
+n=int(input("Enter the number of items"))
+list=[]
+for i in range(n):
+    element=int(input("enter the element"))
+    list.append(element)
+print(list)
+sum=0
+for i in list:
+    sum=sum+i
+print("sum:",sum)
