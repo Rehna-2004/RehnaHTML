69295ded3f40f4e259160c42fece8e82e8ac7194
Rehna-2004 <rehnampta@gmail.com>
2025-10-30 10:33:06 +0530

    Add files via upload

---
+++ prg2.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registration Form</title>
+    </head>
+    <body bgcolor="lavender">
+        
+        <center><h2>Registration Form</h2>
+        <?php
+        // Initialize variables
+
+        $name=$email=$password=$phone="";
+        $nameErr=$emailErr=$passErr=$phoneErr="";
+        //check if form is submitted
+
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+        // Validate Name
+
+            if(empty($_POST["name"])){
+                $nameErr="Name is required";
+            } elseif (!preg_match("/^[A-Z][a-z]{26}$/", $_POST["name"])) {
+                $nameErr="Name must start with a capital letter and contain only letters";
+            } else {
+                $name=$_POST["name"];
+            }
+            // Validate Email
+
+            if(empty($_POST["email"])){
+                $emailErr="Email is required";
+            }elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                $emailErr="Invalid email format";    
+            } else {
+                $email=$_POST["email"];
+            }
+            // Validate Password
+
+            if(empty($_POST["password"])){
+                $passErr="Password is required";
+            } elseif (strlen($_POST["password"])<6) {
+                $passErr="Password must be at least 6 characters";
+            } else {
+                $password=$_POST["password"];
+            }
+            // Validate Phone Number
+
+            if(empty($_POST["phone"])){
+                $phoneErr="Phone number is required";
+            } elseif (!preg_match("/^[0-9]{10}$/", $_POST["phone"])) {
+                $phoneErr="Phone number must be 10 digits";
+            } else {
+                $phone=$_POST["phone"];
+            
+            // If no errors, display success message
+
+
+                if($nameErr=="" && $emailErr=="" && $passErr=="" && $phoneErr=="") {
+                     echo "<h3 style='color: green;'>Registration Successful!</h3>";
+                     echo"Name:$name<br>";
+                     echo"Email:$email<br>";
+                     echo"Phone:$phone<br>";
+            }
+            }
+        }
+        
+        
+        ?>
+        <form method="post" action="">
+            Name: <input type="text" name="name" value="<?php echo $name;?>"><br>
+            <span style="color:red;"><?php echo $nameErr; ?></span><br>
+            Email: <input type="text" name="email" value="<?php echo $email;?>"><br>
+            <span style="color: red;"><?php echo $emailErr; ?></span><br>
+            Password: <input type="password" name="password" value="<?php $password;?>"><br>
+            <span style="color: red;"><?php echo $passErr; ?></span><br>
+            Phone: <input type="text" name="phone" value="<?php echo $phone;?>"><br>
+            <span style="color: red;"><?php echo $phoneErr; ?></span><br>
+            <input type="submit" value="Register">
+        </form>
+    </center>
+    </body>
+</html>
